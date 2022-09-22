951168960be89782b353098d784068ea9dab18ed
Luciano <lucianopsn15@gmail.com>
2022-09-22 18:05:36 +0200

    componentDidMount and many more

--- src/App.js
+++ src/App.js
@@ -1,25 +1,42 @@
-import logo from './logo.svg';
-import './App.css';
+import { Component } from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  // in a class component we use a "constructor" followed by "super()" and we call a useState by using the keyword "this.state"
+  constructor() {
+    super();
+
+    // this is setting a state
+    this.state = {
+      monsters: [],
+    };
+  }
+
+  //this is a component life cycle and its best used when we want a promise like fetch (remote API request)
+  componentDidMount() {
+    //first we fetch data from remote place using 'fetch
+    fetch('https://jsonplaceholder.typicode.com/users')
+      //then we take the response from the site and turn it into json
+      .then((response) => response.json())
+      // then we take the first ".then" and pass "users" and callback "setState" to return users inside our state "monsters" array
+      .then((users) =>
+        this.setState(() => {
+          return { monsters: users };
+        })
+      );
+  }
+
+  render() {
+    return (
+      <div className="App">
+        {/* this is how you callBack of the state. we map how state which in this
+        case it would be the empty array of 'monsters' */}
+        {this.state.monsters.map((monsters) => {
+          // we want the monsters to return a map in a div and we set the key inside the div
+          return <div key={monsters.id}>{monsters.name}</div>;
+        })}
+      </div>
+    );
+  }
 }
 
 export default App;
